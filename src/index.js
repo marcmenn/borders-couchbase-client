41071b78b91576b3cc1ca407779b2a3599c5b9c4
Oliver Esser <oliver.esser@actano.de>
2018-03-27 11:23:20 +0200

    Export command name constants for views
    In order to expand the couchbase backend the view constants are needed
    to define decorated actions for view commands.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 export * from './commands'
 export { default as getBucket } from './get-bucket-command'
 export { default } from './multi-bucket'
-export { queryViewAsArray, queryViewAsAsyncIterator, queryViewAsStream, key, id, value } from './view'
+export {
+  queryViewAsArray,
+  queryViewAsAsyncIterator,
+  queryViewAsStream,
+  key,
+  id,
+  value,
+  VIEW_AS_STREAM,
+  VIEW_AS_ASYNC_ITERATOR,
+  VIEW_AS_ARRAY,
+} from './view'
