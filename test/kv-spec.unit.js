8ce308f4cd29fdec228d2ea912c5a5eeea6bdef4
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-10 14:59:47 +0200

    Use borders multi backend syntax

--- test/kv-spec.unit.js
+++ test/kv-spec.unit.js
@@ -3,5 +3,5 @@ import testBucketFactory from '../src/mock-bucket-factory'
 import createBackend from '../src/multi-bucket'
 
 describe('data-access-server/couchbase', () => {
-  testKeyValueBackend(() => createBackend(testBucketFactory))
+  testKeyValueBackend(() => [createBackend(testBucketFactory)])
 })
