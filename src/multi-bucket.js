0dd889d2bdb8a64b51299ce0f4a3b7cefc87841a
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-18 18:25:48 +0200

    Drop disconnected buckets

--- src/multi-bucket.js
+++ src/multi-bucket.js
@@ -4,11 +4,16 @@ import viewsBackend from './multi-bucket-views'
 export default (bucketFactory, kvBackendDecorator, viewBackendDecorator) => {
   const buckets = {}
 
-  const _bucketFactory = (bucketName) => {
+  const _bucketFactory = async (bucketName) => {
     if (!buckets[bucketName]) {
       buckets[bucketName] = bucketFactory(bucketName)
     }
-    return buckets[bucketName]
+    const bucket = await buckets[bucketName]
+    if (bucket.connected === false) {
+      buckets[bucketName] = null
+      return _bucketFactory(bucketName)
+    }
+    return bucket
   }
 
   return Object.assign(
