b3e251b5248a4994a65aa42d003a4654b88fee77
thrucker <t.rucker@xpure.de>
2018-10-18 17:20:49 +0200

    Add exports for backends

---
+++ src/backends/index.js
@@ -0,0 +1,12 @@
+export {
+  default as KeyValueBackend,
+  SUPPORTED_COMMANDS as KV_COMMANDS,
+} from './key-value'
+export {
+  default as UpsertViewsBackend,
+  SUPPORTED_COMMANDS as UPSERT_VIEWS_COMMANDS,
+} from './upsert-views'
+export {
+  default as ViewsBackend,
+  SUPPORTED_COMMANDS as VIEWS_COMMANDS,
+} from './views'
